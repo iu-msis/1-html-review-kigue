520160f6b31765c4c4c3c27bf828826fb98b1532
kigue <65794738+kigue@users.noreply.github.com>
2021-09-29 08:40:00 -0400

    changes

---
+++ app/public/api/test.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$num = 2;
+$foo = "To be";
+$bar = "or not to be";
+
+
+echo $foo ." ". $bar;
+
+/*if (TRUE) {
+	echo "TRUE"
+}
+else{
+	echo "FALSE"
+}
+*/
+
+$arr = [1,1,2,3,5,8];
+$arr2 = ["a", "b", "c"];
+
+echo json_encode($arr2);
