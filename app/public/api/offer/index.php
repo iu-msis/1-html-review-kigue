32e1a5f21765140f410a11efeaabf3ac5d12ce9e
kigue <65794738+kigue@users.noreply.github.com>
2021-10-11 18:02:54 -0400

    changes

--- app/public/api/offer/index.php
+++ app/public/api/offer/index.php
@@ -9,11 +9,6 @@ $db = DbConnection::getConnection();
 $sql = 'SELECT * FROM offer';
 $vars = [];
 
- if (isset($_GET['student'])) {
-   // This is an example of a parameterized query
-   $sql = 'SELECT * FROM offer WHERE studentId = ?';
-   $vars = [ $_GET['student'] ];
- }
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
