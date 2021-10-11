c96d017a72c2a84af9f4d120ac1d048933a92939
kigue <65794738+kigue@users.noreply.github.com>
2021-10-11 16:20:22 -0400

    changes

--- app/public/api/books/index.php
+++ app/public/api/books/index.php
@@ -6,24 +6,24 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM books';
+$sql = 'SELECT * FROM offer';
 $vars = [];
 
  if (isset($_GET['student'])) {
    // This is an example of a parameterized query
-   $sql = 'SELECT * FROM books WHERE studentId = ?';
+   $sql = 'SELECT * FROM offer WHERE studentId = ?';
    $vars = [ $_GET['student'] ];
  }
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$books = $stmt->fetchAll();
+$patients = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($books, JSON_PRETTY_PRINT);
+$json = json_encode($patients, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
 echo $json;
-		
+    
--- app/public/data/books.sql
+++ app/public/data/books.sql
@@ -1,29 +1,34 @@
 CREATE DATABASE IF NOT EXISTS msisdb;
 USE msisdb;
 
-
--- SELECT * FROM books;
-
 DROP TABLE IF EXISTS books;
-CREATE TABLE books (
-	id int PRIMARY KEY AUTO_INCREMENT,
-    studentId int NOT NULL REFERENCES student(id) 
-        ON DELETE CASCADE ON UPDATE CASCADE,
-	companyName VARCHAR(24) NOT NULL DEFAULT '',
-    salary int NOT NULL DEFAULT 0,
-    bonus int NOT NULL DEFAULT 0,
-	offerDate date NOT NULL DEFAULT(CURRENT_DATE)
+
+create table books (
+  id INT,
+  title VARCHAR(50),
+  author VARCHAR(50),
+  year_pub VARCHAR(50),
+  publisher VARCHAR(50),
+  page_count INT,
+  MSRP VARCHAR(50)
 );
-
--- Student 1 has no offers, Student 2 has 3 offers, Student 3 has one offer
-INSERT INTO books(id, studentId, companyName, salary, bonus, offerDate) VALUES
-  (1, 2, 'KPMG', 95000, 7000, '2021-09-30'),
-  (2, 2, 'Deloitte Digital', 94000, 12000, '2021-10-03'),
-  (3, 2, 'IU, ISGP', 54000, 0, '2021-10-05'),
-  (4, 3, 'Amazon', 122000, 11000, '2021-10-15')
-;
-
--- COMMIT;
-
--- CREATE USER 'msisreader'@'%' IDENTIFIED BY 'msisreadonly';
--- GRANT SELECT ON * . * TO 'msisreader'@'%';
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (1, 'Bel Ami', 'Mala Smallpeace', 2008, 'Ziemann Group', 262, '$2.55');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (2, 'Rugrats Movie, The', 'Wrennie Pennetta', 2004, 'Hintz-Jast', 738, '$1.45');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (3, 'Zero Years, The', 'Nicki Lomb', 2012, 'Legros Group', 217, '$4.96');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (4, 'Woman Chaser, The', 'Merrilee Escofier', 1995, 'Hyatt LLC', 454, '$2.97');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (5, 'Megiddo: The Omega Code 2', 'Cristy Steels', 2005, 'Halvorson Inc', 256, '$9.29');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (6, 'Last Life in the Universe (Ruang rak noi nid mahasan)', 'Ashia Quaintance', 1993, 'Corkery, Bernier and Nitzsche', 999, '$7.34');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (7, 'Unmarried Woman, An', 'Eulalie Goman', 1998, 'Lockman Group', 208, '$9.22');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (8, 'Wild Love', 'Gloria Riping', 2011, 'Considine-Durgan', 535, '$2.04');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (9, 'Belle of the Nineties', 'Nona Vader', 2000, 'Yost, Kuvalis and Sipes', 551, '$9.85');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (10, 'Color of Friendship, The', 'Shane Blay', 2011, 'Emmerich, Hermann and Wyman', 264, '$6.57');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (11, 'People That Time Forgot, The', 'Debora Monahan', 2011, 'White and Sons', 776, '$7.60');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (12, 'Chronicle of the Years of Fire (Chronique des années de braise)', 'Sumner Winnard', 1997, 'Hirthe Group', 861, '$9.20');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (13, 'Private Lessons', 'Beilul Channer', 2003, 'Cormier-Schiller', 598, '$4.24');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (14, 'Bank Error in Your Favour (Erreur de la banque en votre faveur)', 'Gran Warnock', 2010, 'Franecki, Stehr and Huels', 792, '$3.23');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (15, 'Snarveien (Detour)', 'Nona Pasley', 2004, 'Pagac Inc', 851, '$3.05');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (16, 'See How They Fall (Regarde les hommes tomber)', 'Augustine Bareham', 2002, 'Yost, Waelchi and Crist', 589, '$6.31');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (17, 'I Am My Films - A Portrait of Werner Herzog (Was ich bin, sind meine Filme)', 'Drew Holhouse', 1994, 'Hansen, Kiehn and Schowalter', 759, '$1.90');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (18, 'Blonde and Blonder', 'Tait Westhoff', 2006, 'Turcotte, Mante and Marvin', 110, '$4.83');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (19, 'Sign of Four, The', 'Georgi Lammertz', 2007, 'Mohr, Barrows and Kris', 768, '$2.77');
+insert into books (id, title, author, year_pub, publisher, page_count, MSRP) values (20, 'Family Guy Presents: Blue Harvest', 'Sharona Epsley', 2008, 'Schaefer Group', 849, '$5.89');
